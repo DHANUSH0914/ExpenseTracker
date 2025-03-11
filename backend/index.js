2ab73879ceeed2f486aa7da12b2fabfaecaab1af
DHANUSH0914 <dhanush0914@gmail.com>
2025-03-11 12:13:49 +0530

    added backend

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express=require("express");
+const cors=require("cors");
+const mongoose=require("mongoose");
+const dotenv=require("dotenv")
+const expenseRoute=require("./routes/expense")
+
+dotenv.config()
+const app=express();
+
+
+
+//middleware
+app.use(cors());
+app.use(express.json())
+
+//Routes
+app.use("/expenses",expenseRoute);
+
+
+//DB connection
+
+mongoose.connect(process.env.DB_CONNECTION).then(()=>{
+    console.log("DataBase is Connection is Established Successfully");
+}).catch((err)=>{
+    console.log("not established");
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
---
+++ backend/routes/expense.js
@@ -0,0 +1,62 @@
+const express=require("express");
+const router=express.Router();
+const Expense=require("../models/Expense");
+
+// ADD EXPENSE 
+
+router.post("/",async(req,res)=>{
+    console.log(req.body);
+    try{
+        const newExpense=await Expense(req.body);
+        const expense=newExpense.save();
+        res.status(201).json(expense);
+
+    }catch(error){
+        res.status(500).json(error);
+    }
+})
+
+// Get Expense from DB
+
+router.get("/",async(req,res)=>{
+  
+    try {
+        const expenses=await Expense.find().sort({createdAt:-1}); //mongodb func
+        res.status(200).json(expenses)
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+//update Expense
+
+router.put("/:id",async(req,res)=>{
+    try {
+        const expense=await Expense.findByIdAndUpdate(
+            req.params.id,                                    //pull the Id from the Given DataBase
+            {
+                $set:req.body
+            },
+            {new:true}
+        );
+        res.status(201).json(expense)
+        
+    } catch (error) {
+        res.status(500).json(error);
+    }
+
+})
+
+//Delete an Expense
+
+router.delete("/:id",async(req,res)=>{
+    try {
+        await Expense.findByIdAndDelete(req.params.id);
+        res.status(201).json("Deleted Successfully")
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+module.exports=router;
